57646b340e3d0f9b2219cba3be913039032f8b86
Addy-A <mardol69@gmail.com>
2024-12-03 18:37:38 -0600

    intial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config({ path: __dirname + "/.env" });
+const { twitterClient } = require("./twitterClient.js")
+const CronJob = require("cron").CronJob;
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+  })
+  
+const tweet = async () => {
+    try {
+        await twitterClient.v2.tweet("Hello world!");
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const cronTweet = new CronJob("30 * * * * *", async () => {
+    tweet();
+});
+
+cronTweet.start();
